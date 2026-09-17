b7b39e5f27fcfcfbd76cc1143dd5179678451209
RyanKibz <ryan.serem@students.moringaschool.com>
2026-09-17 07:52:27 +0300

    music system done

--- lib/song.py
+++ lib/song.py
@@ -1,57 +1,45 @@
+#!/usr/bin/env python3
+
+
 class Song:
-    # Class Attributes
+    # class-level attributes shared across every Song instance
     count = 0
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
-    artist_count = {}  # Added to match test suite expectations
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
-        """Initialize instance attributes and trigger class methods."""
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        # Trigger class methods upon initialization
-        Song.add_song_to_count()
-        Song.add_to_genres(genre)
-        Song.add_to_artists(artist)
-        Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
+        # every time a new song gets made, update the class-wide tracking
+        self.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
 
-    @classmethod
-    def add_song_to_count(cls):
-        """Increments total song count by 1."""
-        cls.count += 1
+    def add_song_to_count(self):
+        Song.count += 1
 
-    @classmethod
-    def add_to_genres(cls, genre):
-        """Adds unique genres to the genres list."""
-        if genre not in cls.genres:
-            cls.genres.append(genre)
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
 
-    @classmethod
-    def add_to_artists(cls, artist):
-        """Adds unique artists to the artists list."""
-        if artist not in cls.artists:
-            cls.artists.append(artist)
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
 
-    @classmethod
-    def add_to_genre_count(cls, genre):
-        """Updates frequency of songs per genre."""
-        if genre in cls.genre_count:
-            cls.genre_count[genre] += 1
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
         else:
-            cls.genre_count[genre] = 1
+            Song.genre_count[self.genre] = 1
 
-    @classmethod
-    def add_to_artists_count(cls, artist):
-        """Updates frequency of songs per artist."""
-        if artist in cls.artists_count:
-            cls.artists_count[artist] += 1
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
         else:
-            cls.artists_count[artist] = 1
-            
-        # Update singular attribute to satisfy test suite
-        cls.artist_count[artist] = cls.artists_count[artist]
+            Song.artist_count[self.artist] = 1
